c1b6e01c1ecfe9e81f85248e374525655caeebe9
cover-del <a0908881722@gmail.com>
2026-01-06 15:07:48 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
 
   try {
     const GAS_URL =
-      'https://script.google.com/macros/s/AKfycbwmctrwxSUNmSEgAs5FcVI9eEGoFUzm2h1rks5QYcO0bQsrJpVKr4w4hHwH6mfvrVYX/exec';
+      'https://script.google.com/macros/s/AKfycbyMPOjB_rrDrnGGnl3JD4fKgFFnDLOKNy7cjQxy3xgbxaPjiLBAXFFRAqPV1kA-BGcM/exec';
 
     const gasRes = await fetch(GAS_URL, {
       method: 'POST',
